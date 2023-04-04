954551c05c6549e9654dcb24ddceb9559c389724
LuisFelipe333 <luigifelipe333@gmail.com>
2023-04-03 21:57:34 -0600

    Interface in Client side added

--- Ben10AliensApp/Client/Services/AlienService/AlienService.cs
+++ Ben10AliensApp/Client/Services/AlienService/AlienService.cs
@@ -1,7 +1,42 @@
-﻿namespace Ben10AliensApp.Client.Services.AlienService
+﻿using Ben10AliensApp.Shared.Models;
+using System.Net.Http.Json;
+
+namespace Ben10AliensApp.Client.Services.AlienService
 {
     public class AlienService : IAlienService
     {
+        private readonly HttpClient http;
 
+        public AlienService(HttpClient http)
+        {
+            this.http = http;
+        }
+
+        public List<Alien> Aliens { get; set; } = new List<Alien>();
+
+        public Task CreateAlien(Alien alien)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAlien(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Alien?> GetAlienById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task GetAliens()
+        {
+            var result = await http.GetFromJsonAsync<List<Alien>>("api/aliens");
+        }
+
+        public Task UpdateAliens(int id, Alien alien)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Ben10AliensApp/Client/Services/AlienService/IAlienService.cs
+++ Ben10AliensApp/Client/Services/AlienService/IAlienService.cs
@@ -1,6 +1,16 @@
-﻿namespace Ben10AliensApp.Client.Services.AlienService
+﻿using Ben10AliensApp.Shared.Models;
+
+namespace Ben10AliensApp.Client.Services.AlienService
 {
     public interface IAlienService
     {
+        List<Alien> Aliens { get; set; }
+
+        Task GetAliens();
+        Task<Alien?> GetAlienById (int id);
+        Task CreateAlien(Alien alien);
+        Task UpdateAliens(int id, Alien alien);
+        Task DeleteAlien(int id);
+
     }
 }
