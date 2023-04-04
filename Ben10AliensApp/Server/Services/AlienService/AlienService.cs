36c55c492068f6506bb447a72d3a4d24ab876b94
LuisFelipe333 <luigifelipe333@gmail.com>
2023-04-03 21:58:53 -0600

    Interface in Server side added

---
+++ Ben10AliensApp/Server/Services/AlienService/AlienService.cs
@@ -0,0 +1,32 @@
+﻿using Ben10AliensApp.Shared.Models;
+
+namespace Ben10AliensApp.Server.Services.AlienService
+{
+    public class AlienService : IAlienService
+    {
+        public Task<Alien> CreateAlien(Alien alien)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAlien(int alienId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Alien?> GetAlienById(int alienId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Alien>> GetAliens()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Alien?> UpdateAlien(int alienId, Alien alien)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ben10AliensApp/Server/Services/AlienService/IAlienService.cs
@@ -0,0 +1,13 @@
+﻿using Ben10AliensApp.Shared.Models;
+
+namespace Ben10AliensApp.Server.Services.AlienService
+{
+    public interface IAlienService
+    {
+        Task<List<Alien>> GetAliens();
+        Task<Alien?> GetAlienById(int alienId);
+        Task<Alien> CreateAlien(Alien alien);
+        Task<Alien?> UpdateAlien(int alienId, Alien alien);
+        Task<bool> DeleteAlien (int alienId);
+    }
+}
